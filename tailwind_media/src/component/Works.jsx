df8f1d7857649fcab81ee64b3be91ef53befcb13
Ankit Singh <ankitsinghasingh007@gmail,com>
2024-04-17 12:26:50 +0530

    worksImageBugFixed

--- tailwind_media/src/component/Works.jsx
+++ tailwind_media/src/component/Works.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import B1 from '../assets/B1.png'
+import B2 from "../assets/B2.png";
+import B3 from "../assets/B3.png";
 
 function works() {
   return (
@@ -14,7 +16,7 @@ function works() {
         <div className="flex justify center md:w-1/2 ">
           <img
             className="md:w-[300px] md:h-[300px] w-44 mx-10 object-cover"data-aos='slide-right'
-            src="https://s3-alpha-sig.figma.com/img/b0b3/a9ad/91b0a75dd8facad6dc7967201d798a95?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nAZdtlAfyY72Yug0zXcNr8aE8ndJzJGFOOUdJxLW2g5JGNwY7lVZDxkPuTuM2fnm5z2Tsj1UsyXMXj6NCdFvQYPEfT38lDoR96e2zttu7aoIb9vYkeFaYsYat5AzlrOJpEXWY2iqbRXH64dCm2HUK8QKSkm-7PnLJ-0udgca-UNsMDMic4PojF7WjwI56EyfUoONWdhPJFBFtoEFpFxscXXjT74wdG8mkw4IS0zBw7WRWLj1xv0h7URyWN-1-PjojVX~RJDeSZKX0xOj24usmyBdebDOLnRLg7CIeiY9T6MAj63Qv9lBAA64fEaYYSoKxmREYyHvsdPoxDxN4yjUVg__"
+            src={B1}
             alt=""/>
           
           <div className="w-1/2  text-white md:text-4xl  font-mono font-semibold flex flex-col items-center justify-center" data-aos='slide-left'>
@@ -27,7 +29,7 @@ function works() {
         <div className="flex my-20 w-1/2 md:px-20">
           <img
             className="md:w-[300px] md:h-[300px] w-44 -mx-12 object-cover"data-aos='slide-right'
-            src="https://s3-alpha-sig.figma.com/img/c1d5/de02/cd6d79a358292bc059279751c2f3ffde?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cisjbM64GNMzrYWctgndJF~oDUj-GrPlU0SISkfaB7gXK47UZDn7j8HAthSgdZvRS78Yjwu3TDrtFG2sDGfF9bmZSSDZ0FO4l3ZeJvt8gGSRvX4rb~cFplW6QsK4M8NwuVKqyZ0wD1l4bLb0gBGcofr9LhkhP4XvUlL97tvD9iSqP-ilUpDi0uOQDm39lmdGjV-7fdrqKRDHdLl9rU6I12q9wW78W8u5lwgFNXxzC2J9QluOKpoaYWiaMI1COL5Hakd39nRNxIHimhrlAloQx4q4eV6NPs44asm7pnrWnGwB1gg8583H2xU637-JNWA3F2p6fMqWigymp98igI5q8g__"
+            src={B2}
             alt=""
           />
           <div className="w-1/2  justify-start text-white md:text-4xl  font-mono font-semibold flex flex-col items-center mx-24 "  data-aos='slide-left'>
@@ -40,7 +42,7 @@ function works() {
         <div className="flex  w-1/2 md:px-20">
           <img
             className="md:w-[300px] md:h-[300px] w-44 -mx-12 object-cover"data-aos='slide-right'
-            src="https://s3-alpha-sig.figma.com/img/42e4/82a3/ec05367bc53771dc975864f0eaa65cd5?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mjQy2~s~F9krTxCxBaKL~Zk4mGRMXMdgf9C0dBaQ-HtsE3Kw~455XdPit7fwOU83DbPaHb06USWvLbI0gucUvRjaDIEIafbquBkUb5-50IrP0Zcjjy6ZcqeCiTqgcvRsF7kIGlpXjrVhyyzHfqADSpANApTVJqBqo0PqTx8ifHC3twQWmYfqmoqndmwEvrsK2cP35PKEejviAp65R6mCevPzIGOj0A88fFmUp6OVEqz3Lmdw-heZJqHEJme9RvcjfMyP-m1ab46PS4ChJznvfuckzUzOcLf~K2mRubF-dqaQCPcpdxN4q1gQ-Ku5-IndBPa8Ad9It5wC2tjQHFlZEA__"
+            src={B3}
             alt=""
           />
           <div className="w-1/2  text-white md:text-4xl font-mono font-semibold flex flex-col items-center justify-center mx-24 " data-aos='slide-left'>
