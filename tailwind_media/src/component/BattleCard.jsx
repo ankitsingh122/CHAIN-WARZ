98344e422cd3ce17a9c500af7f227888b05825e9
Ankit Singh <ankitsinghasingh007@gmail,com>
2024-04-30 13:00:08 +0530

    UsedProps

---
+++ tailwind_media/src/component/BattleCard.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import ETH from "../assets/ETH.png"; 
+
+import { useState, useEffect } from "react";
+function BattleCard(props) {
+      const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+      useEffect(() => {
+        const timer = setTimeout(() => {
+          setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+      });
+
+      function calculateTimeLeft() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+        const nextResetHours = hours < 12 ? 12 : 24;
+        const hoursLeft = nextResetHours - (hours % 12);
+        const minutesLeft = 59 - minutes;
+        const secondsLeft = 59 - seconds;
+
+        return {
+          hours: hoursLeft,
+          minutes: minutesLeft,
+          seconds: secondsLeft,
+        };
+      }
+
+
+  return (
+    <>
+      
+        <div className="border mt-5" data-aos="slide-right">
+          <div className="flex justify-center mt-3 ml-3">
+            <img className="" src={props.img} alt="" /> 
+          </div>
+          <div className="flex justify-center mt-5">
+            <div className="text-white ">
+              ENTRY
+              <br />
+              PRICE
+            </div>
+            <img src={ETH} alt="" /> 
+            <div className="text-white mt-3">0.0001</div>
+          </div>
+          <div className="flex justify-center">
+            <div className="bg-gradient-to-tr from-red-500 via-purple-500 to-blue-500 px-3 py-1 rounded-xl mt-3 flex">
+              <div className="text-white">
+                PRIZE <br />
+                POOL
+              </div>
+              <img src={ETH} alt="" />
+              <div className="text-white mt-3">0.0000</div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="flex bg-white px-12 py-2 rounded-xl mt-3 text-xl cursor-pointer ">
+              Enter
+            </div>
+          </div>
+          <div className="flex justify-center mx-10 mt-5">
+            <div className="flex justify-center font-azonix md:text-4xl text-3xl md:px-10 px-6 py-2 border-gradient border-4 bg-black text-white">
+              <div>{`${timeLeft.hours
+                .toString()
+                .padStart(2, "0")}:${timeLeft.minutes
+                .toString()
+                .padStart(2, "0")}:${timeLeft.seconds
+                .toString()
+                .padStart(2, "0")}`}</div>
+            </div>
+          </div>
+          <div className="text-blue-600 flex justify-center mt-5">
+            CLOSES IN
+          </div>
+        </div>
+      
+    </>
+  );
+}
+
+export default BattleCard;
--- tailwind_media/src/pages/BossFight.jsx
+++ tailwind_media/src/pages/BossFight.jsx
@@ -1,38 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import NavBar from "../component/NavBar";
 import { Link } from "react-router-dom";
 import A1 from "../assets/A1.png";
-import ETH from "../assets/ETH.png";
+import BattleCard from "../component/BattleCard";
 import Join from "../component/Join";
 function Battle() {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  });
-
-  function calculateTimeLeft() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-
-    const nextResetHours = hours < 12 ? 12 : 24;
-    const hoursLeft = nextResetHours - (hours % 12);
-    const minutesLeft = 59 - minutes;
-    const secondsLeft = 59 - seconds;
-
-    return {
-      hours: hoursLeft,
-      minutes: minutesLeft,
-      seconds: secondsLeft,
-    };
-  }
-
+ 
   return (
     <div className="bg-custom w-screen h-full font-azonix">
       <div className="flex justify-center">
@@ -71,49 +44,7 @@ function Battle() {
         </Link>
       </div>
       <div className="lg:flex justify-center  mx-2 lg:space-x-5 mt-20">
-        <div className="border mt-5" data-aos="slide-right">
-          <div className="flex justify-center mt-3 ml-3">
-            <img className="" src={A1} alt="" />
-          </div>
-          <div className="flex justify-center mt-5">
-            <div className="text-white">
-              ENTRY
-              <br />
-              PRICE
-            </div>
-            <img src={ETH} alt="" />
-            <div className="text-white mt-3">0.0001</div>
-          </div>
-          <div className="flex justify-center">
-            <div className="bg-gradient-to-tr from-red-500 via-purple-500 to-blue-500 px-3 py-1 rounded-xl mt-3 flex">
-              <div className="text-white">
-                PRIZE <br />
-                POOL
-              </div>
-              <img src={ETH} alt="" />
-              <div className="text-white mt-3">0.0000</div>
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <div className="flex bg-white px-12 py-2 rounded-xl mt-3 text-xl cursor-pointer">
-              Enter
-            </div>
-          </div>
-          <div className="flex justify-center mx-10 mt-5">
-            <div className="flex justify-center font-azonix md:text-4xl text-3xl md:px-10 px-6 py-2 border-gradient border-4 bg-black text-white">
-              <div>{`${timeLeft.hours
-                .toString()
-                .padStart(2, "0")}:${timeLeft.minutes
-                .toString()
-                .padStart(2, "0")}:${timeLeft.seconds
-                .toString()
-                .padStart(2, "0")}`}</div>
-            </div>
-          </div>
-          <div className="text-blue-600 flex justify-center mt-5">
-            CLOSES IN
-          </div>
-        </div>
+       <BattleCard img={A1} />
       </div>
       <Join />
     </div>
